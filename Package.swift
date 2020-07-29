e8b5326992703941a2b549edf43a38701e7105a3
capkovicm <capkovicm@users.noreply.github.com>
2020-07-30 00:42:58 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,10 +4,22 @@
 //
 //  Created by Satyam Ghodasara on 1/23/16.
 //
-//
+// swift-tools-version:5.0
 
 import PackageDescription
 
 let package = Package(
     name: "ForecastIO"
 )
+let package = Package(name: "ForecastIO",
+                      platforms: [.macOS(.v10_10),
+                                  .iOS(.v10),
+                                  .tvOS(.v9)],
+                      products: [.library(name: "ForecastIO",
+                                          targets: ["ForecastIO"])],
+                      targets: [.target(name: "ForecastIO",
+                                        path: "Source"),
+                                .testTarget(name: "ForecastIOTests",
+                                            dependencies: ["ForecastIO"],
+                                            path: "Tests")],
+                      swiftLanguageVersions: [.v5])

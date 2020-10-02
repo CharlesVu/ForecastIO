5f8667b1fcc5afd03a01c1a34b384780ce835f6a
Charles Vu <753278+CharlesVu@users.noreply.github.com>
2020-10-02 10:41:58 +0100

    Update Package.swift file to support SwiftPM 5.3

--- Package.swift
+++ Package.swift
@@ -1,25 +1,22 @@
-//
-//  Package.swift
-//  ForecastIO
-//
-//  Created by Satyam Ghodasara on 1/23/16.
-//
-// swift-tools-version:5.0
+// swift-tools-version:5.3
 
 import PackageDescription
 
 let package = Package(
-    name: "ForecastIO"
+    name: "ForecastIO",
+    platforms: [.macOS(.v10_10),
+                .iOS(.v10),
+                .tvOS(.v9)],
+    products: [.library(name: "ForecastIO",
+                        targets: ["ForecastIO"])],
+    dependencies: [
+        .package(url: "https://github.com/AliSoftware/OHHTTPStubs",
+                 from: "9.0.0"),
+    ],
+    targets: [.target(name: "ForecastIO",
+                      path: "Source"),
+              .testTarget(name: "ForecastIOTests",
+                          dependencies: ["ForecastIO", "OHHTTPStubs"],
+                          path: "Tests")],
+    swiftLanguageVersions: [.v5]
 )
-let package = Package(name: "ForecastIO",
-                      platforms: [.macOS(.v10_10),
-                                  .iOS(.v10),
-                                  .tvOS(.v9)],
-                      products: [.library(name: "ForecastIO",
-                                          targets: ["ForecastIO"])],
-                      targets: [.target(name: "ForecastIO",
-                                        path: "Source"),
-                                .testTarget(name: "ForecastIOTests",
-                                            dependencies: ["ForecastIO"],
-                                            path: "Tests")],
-                      swiftLanguageVersions: [.v5])
